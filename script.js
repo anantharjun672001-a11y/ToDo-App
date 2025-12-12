6582b601500c12ec9e646b8637a5c51d82efba3f
anantharjun672001-a11y <anantharjun672001@gmail.com>
2025-12-12 18:53:07 +0530

    script steps added

---
+++ script.js
@@ -0,0 +1,13 @@
+//! Getting all Elements from html
+
+//! Form Validation
+
+//! Create Function used to create the data from local storage and display it in my todo's
+
+//! resetting the form after displaying the task
+
+//! Edit function for created TODO'S
+
+//to remove the old task after edited
+
+//! Delete Function for created Todo's
